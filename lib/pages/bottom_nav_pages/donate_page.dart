d143c73799d1b233668902d1eef0269c6393327e
Oyebliss <oyebisi695@gmail.com>
2024-05-11 22:28:29 +0100

    Created a Bottom_Nav_Bar, and navigation through its pages.

---
+++ lib/pages/bottom_nav_pages/donate_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../styles/colors.dart';
+import '../../styles/types.dart';
+
+class DonatePage extends StatelessWidget {
+  const DonatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.Color_shades_white,
+      // AppBar
+      appBar: AppBar(
+        backgroundColor: AppColors.Color_shades_white,
+        automaticallyImplyLeading: false,
+        title: Center(
+          child: Text(
+            'Career',
+            style: AppTextStyles.type_bold_h6,
+          ),
+        ),
+      ),
+
+      // Career body page
+      body: Center(
+        child: Text(
+          'Welcome to the donation page, what will you to be sharing with us as donations 😁',
+          textAlign: TextAlign.center,
+          style: AppTextStyles.type_Medium_body_text4,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/bottom_nav_pages/project_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../styles/colors.dart';
+import '../../styles/types.dart';
+
+class ProjectPage extends StatelessWidget {
+  const ProjectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.Color_shades_white,
+      // AppBar
+      appBar: AppBar(
+        backgroundColor: AppColors.Color_shades_white,
+        automaticallyImplyLeading: false,
+        title: Center(
+          child: Text(
+            'Project',
+            style: AppTextStyles.type_bold_h6,
+          ),
+        ),
+      ),
+
+      // Project body page
+      body: Center(
+        child: Text(
+          'Welcome to the project page, choose a project to work on 🧐. ',
+          textAlign: TextAlign.center,
+          style: AppTextStyles.type_Medium_body_text4,
+        ),
+      ),
+    );
+  }
+}
