e7ea9335c5a5f20b026b2f90fd13b94b8b6cb90d
Oyebliss <oyebisi695@gmail.com>
2024-07-05 22:04:38 +0100

    Created a reusable Appbar and bodycontent file to reduce repetitions

--- lib/pages/bottom_nav_pages/project_page.dart
+++ lib/pages/bottom_nav_pages/project_page.dart
@@ -1,33 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_client/styles/reused_appbar.dart';
+import 'package:mobile_client/styles/reused_body_content.dart';
 
 import '../../styles/colors.dart';
-import '../../styles/types.dart';
 
 class ProjectPage extends StatelessWidget {
   const ProjectPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: AppColors.Color_shades_white,
       // AppBar
-      appBar: AppBar(
-        backgroundColor: AppColors.Color_shades_white,
-        title: Center(
-          child: Text(
-            'Project',
-            style: AppTextStyles.type_bold_h6,
-          ),
-        ),
+      appBar: ReusedAppbar(
+        title: 'Project',
       ),
 
       // Project body page
-      body: Center(
-        child: Text(
-          'Welcome to the project page, choose a project to work on 🧐. ',
-          textAlign: TextAlign.center,
-          style: AppTextStyles.type_Medium_body_text4,
-        ),
+      body: ReusedBodyContent(
+        content: 'Welcome to the project page, choose a project to work on 🧐.',
       ),
     );
   }
