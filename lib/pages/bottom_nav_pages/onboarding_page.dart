15dfceea81a407e7e807795a755465f790f11d86
Oyebliss <oyebisi695@gmail.com>
2024-07-06 13:50:17 +0100

    Created a resuable Scaffold

--- lib/pages/bottom_nav_pages/onboarding_page.dart
+++ lib/pages/bottom_nav_pages/onboarding_page.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_client/styles/reused_appbar.dart';
-import 'package:mobile_client/styles/reused_body_content.dart';
-
-import '../../styles/colors.dart';
+import 'package:mobile_client/styles/reused_scaffold.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.Color_shades_white,
-      // AppBar
-      appBar: ReusedAppbar(
-        title: 'Onboarding',
-      ),
-
-      // Onboarding body page
-      body: ReusedBodyContent(
-        content:
-            'Welcome to the onboarding page, this things you need to know about the project 🚀.',
-      ),
+    return const ReusedScaffold(
+      appTitle: 'Onboarding',
+      bodyContent:
+          'Welcome to the onboarding page, this things you need to know about the project 🚀.',
     );
   }
 }
