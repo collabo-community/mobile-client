aec216f661a707cf7900b2d9f6d4dbb7be66d241
Oyebliss <oyebisi695@gmail.com>
2024-07-06 13:50:17 +0100

    Created a resuable Scaffold

--- lib/pages/bottom_nav_pages/donate_page.dart
+++ lib/pages/bottom_nav_pages/donate_page.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_client/styles/reused_appbar.dart';
-import 'package:mobile_client/styles/reused_body_content.dart';
-
-import '../../styles/colors.dart';
+import 'package:mobile_client/styles/reused_scaffold.dart';
 
 class DonatePage extends StatelessWidget {
   const DonatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.Color_shades_white,
-      // AppBar
-      appBar: ReusedAppbar(
-        title: 'Career',
-      ),
-
-      // Career body page
-      body: ReusedBodyContent(
-        content:
-            'Welcome to the donation page, what will you to be sharing with us as donations 😁',
-      ),
+    return const ReusedScaffold(
+      appTitle: 'Donation',
+      bodyContent:
+          'Welcome to the donation page, what will you to be sharing with us as donations 😁',
     );
   }
 }
--- lib/pages/bottom_nav_pages/project_page.dart
+++ lib/pages/bottom_nav_pages/project_page.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_client/styles/reused_appbar.dart';
-import 'package:mobile_client/styles/reused_body_content.dart';
-
-import '../../styles/colors.dart';
+import 'package:mobile_client/styles/reused_scaffold.dart';
 
 class ProjectPage extends StatelessWidget {
   const ProjectPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.Color_shades_white,
-      // AppBar
-      appBar: ReusedAppbar(
-        title: 'Project',
-      ),
-
-      // Project body page
-      body: ReusedBodyContent(
-        content: 'Welcome to the project page, choose a project to work on 🧐.',
-      ),
+    return const ReusedScaffold(
+      appTitle: 'Project',
+      bodyContent:
+          'Welcome to the project page, choose a project to work on 🧐.',
     );
   }
 }
